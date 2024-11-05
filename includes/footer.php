e48a8415e654fc18e3ee45dda3c94a31a10aab6d
adwaithv7 <adwaithdinesh009@gmail.com>
2024-11-05 18:02:25 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<div class="footer">
+            <div class="container">
+                <div class="footer-top">
+                    <h1 style="color: black">Quick Links</h1>
+                     <br />
+                   
+                                            
+                        <li><a href="index.php"><strong style="color: blue">Home</strong></a></li>
+                    
+                        <li><a href="admin/signin.php"><strong style="color: blue">Admin</strong></a></li>
+                        <li><a href="police/signin.php"><strong style="color: blue">Police</strong></a></li>
+                        <li><a href="user/signin.php"><strong style="color: blue">User</strong></a></li>
+                       
+           
+                </div>
+            </div>
+        </div>
