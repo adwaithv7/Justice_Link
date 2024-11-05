f33b5dc668250e7462d975258c0adff4ecbd5b6d
adwaithv7 <adwaithdinesh009@gmail.com>
2024-11-05 17:45:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Justice Link | Home</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<!--theme-style-->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
+<!--//theme-style-->
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+</head>
+<body>
+<div class="header">
+	
+</div>
+<?php include_once('includes/header.php');?>
+
+<!--content-->
+	<div class="content">
+		<div class="container">
+			
+			<div class="content-welcome">
+			
+				<div class="col-md-8 come">
+					<div class=" welcome">
+						<a href="index.php"><img src="images/india.jpg" alt=""></a>
+					</div>
+				</div>
+				<div class="col-md-4 red">
+				<h3>JUSTICE LINK</h3>
+				<ul>
+							<li>Our mission is to empower law enforcement agencies, legal professionals, and the general public with a comprehensive and user-friendly platform that simplifies the management and retrieval of crime records.</li>
+							<br></br>
+							<li>By leveraging technology and data-driven solutions, we aim to enhance the efficiency, accuracy, and integrity of the criminal justice system.</li>
+							<br></br>
+							<li>We envision a world where crime record management is streamlined, accessible, and transparent, enabling swift justice and safer communities.</li>
+							<li>We envision a world where crime record management is streamlined, accessible, and transparent, enabling swift justice and safer communities.</li>
+							<li></li>
+							<li></li>
+						
+							
+						</ul>
+				</div>
+				<div class="clearfix"> </div>
+		
+			</div>
+			<!---->
+		
+			<!---->
+		</div>
+		
+		<!--footer-->
+		<?php include_once('includes/footer.php');?>
+		<!--//footer-->
+</body>
+</html>
